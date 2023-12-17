50a3f2077704e967eafaaeca957ff17cf3067465
ecarchi- <ecarchi-@student.42porto.com>
2023-12-17 21:54:29 +0000

    added c01

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecarchi- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 11:05:22 by ecarchi-          #+#    #+#             */
+/*   Updated: 2023/12/16 13:59:26 by ecarchi-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int	main()
+{
+	int 	i;
+	i = 0;
+	printf("Before: %d\n", i);
+
+	int *pointer_one = &i;
+	int **pointer_two = &pointer_one;
+	int ***pointer_three = &pointer_two;
+	int ****pointer_four = &pointer_three;
+	int *****pointer_five = &pointer_four;
+	int ******pointer_six = &pointer_five;
+	int *******pointer_seven = &pointer_six;
+	int ********pointer_eight = &pointer_seven;
+
+	ft_ultimate_ft(&pointer_eight);
+	printf("After: %d\n", i);
+
+}*/
