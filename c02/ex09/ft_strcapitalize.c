ef73c8868d46239bc0294ed5023b4ae82facb2ec
Lehendss <edisonfcm1@gmail.com>
2024-01-18 13:38:53 +0000

    Added c09

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,26 @@
+char    *ft_strcapitalize(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+    {
+        if ((str[i - 1] < 47) && ((str[i] >= 'a') && (str[i] <= 'z')))
+        {
+            str[i] -= 32;
+        }
+        i++;
+    }
+    return (str);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char    str1[] = "ola, tudo bem? 42palavras guarenta-e-duas; cinquenta+e+um";
+
+    printf("%s\n", ft_strcapitalize(str1));
+
+}
